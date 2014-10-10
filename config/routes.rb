bd8aa21571e5212ee1477fc2fcaa127a63b98728
Sebastian Vizcaino <elvizcacho@gmail.com>
2014-10-10 00:06:53 -0500

    Listo para pruebas antes de entregar

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'home/index'
   post 'home/crear'
   post 'home/eliminar'
-  
+  match '/' => "home#index", :via => "GET"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
